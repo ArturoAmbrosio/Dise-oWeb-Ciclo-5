1ffae8fd3a9bbdb7731cfa505a4fe8327ad0b495
ArturoAmbrosio <turobrosio@gmail.com>
2021-05-31 10:07:16 -0500

    commit 12

---
+++ Sesión 17/js/misprogramas.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("a").click(function(evento){
+        alert("Has pulsado el enlace.Ahora saldrás a la página de la USAT");
+    });
+});
+
+//ocultar o mostrar una caja
+$("#c1b").click(function(){
+    $("#cont1").toggle(1500);
+}, function(){
+    $("#cont1").toggle(1500);
+})
